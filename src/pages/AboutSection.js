c659a6905f794cd7a196519505d90ac7314da195
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-12-30 23:05:01 -0500

    added images

--- src/pages/AboutSection.js
+++ src/pages/AboutSection.js
@@ -1,10 +1,13 @@
 import React from "react";
+import LogImage from "../assets/logimage.jpg";
+import "./AboutUs.css";
 
 function AboutSection() {
   return (
     <section>
-      <h2>About Us</h2>
-      <p>This is the about section.</p>
+      {/* <img src={LogImage} alt="Hunter Website Logo" className="logimage" /> */}
+      {/* <h2>About Us</h2> */}
+      {/* <p>This is the about section.</p> */}
     </section>
   );
 }
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -6,8 +6,7 @@ import "./footer.css"; // Make sure to import the CSS
 function Footer() {
   return (
     <footer className="footer">
-      {/* Left Black Box */}
-      <div className="footer-left-box">
+      <div>
         <p className="footer-copyright">
           &copy; 2024 HUNTER TERMITE AND PEST MGMT
         </p>
@@ -16,15 +15,14 @@ function Footer() {
         </p>
       </div>
 
-      {/* Right Black Box */}
-      <div className="footer-right-box">
-        <div className="footer-texts">
-          <p className="footer-phone">
-            <a href="sms:+14132213065" className="text-button">
-              Call/Text (413) 221-3065
-            </a>
-          </p>
-        </div>
+      <div className="footer-texts">
+        <p className="footer-phone">
+          <a href="sms:+14132213065" className="text-button">
+            Call/Text (413) 221-3065
+          </a>
+        </p>
+      </div>
+      <div className="footer-instagram">
         <a
           href="https://www.instagram.com/huntertermite/"
           target="_blank"
