0efcfa284fe23d4d112f42f71171ccda308ad133
Ivana Carrillo <ivanae.carrillo@gmail.com>
2025-01-10 20:23:51 -0500

    footer is css responsive

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from "react";
 import HeroSection from "./pages/HeroSection";
 import Footer from "./pages/Footer";
 import "./App.css";
-
 const App = () => {
   return (
     <div className="app-container">
-      {/* HeroSection */}
       <HeroSection />
 
       <Footer />
